d413ca766085729e1d544dcc47f545b330688e40
dana <dana>
2026-03-28 15:24:42 +0300

    Complete Part 2: countVowels, isPalindrome, and treeToSentence with recursion

--- src/part2.ts
+++ src/part2.ts
@@ -4,10 +4,16 @@ const stringToArray = R.split("");
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: (s: string) => number = undefined as any;;
+const vowelsOnly:(s: string[])=>string[]= R.filter((val:string):boolean=>R.includes(val,vowels));
+export const countVowels: (s: string) => number =R.pipe(R.toLower,stringToArray,vowelsOnly,R.length);
 
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
+const clean: (s:string)=> string= R.pipe(R.toLower, R.replace(/[^a-z0-9]/g, ''));
+const reversea:(s: string)=>string=R.pipe(stringToArray, R.reverse,R.join(""));
+const reversestr:(s:string)=> string=R.pipe(clean,reversea);
+
+export const isPalindrome = (text: string): boolean => R.equals(clean(text), reversestr(text));
+
   
 
 /* Question 2.3 */
@@ -16,4 +22,6 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+
+const preparearray:(t:WordTree)=> string[]=(t: WordTree): string[] => R.prepend(t.root, R.chain(preparearray, t.children))
+export const treeToSentence = (t: WordTree): string => R.pipe(preparearray,R.filter((s: string) => s.length > 0), R.join(" "))(t);
